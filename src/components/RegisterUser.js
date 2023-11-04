4056e099579539895716941742c677ffb75fc03b
JuanDBta <simplebetsas@gmail.com>
2023-11-04 12:47:58 -0500

    Update RegisterUser component

--- src/components/RegisterUser.js
+++ src/components/RegisterUser.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../redux/features/users/usersSlice';
-import '../styles/Login.css';
+import '../../assets/stylesheets/login.css';
 
 const RegisterUser = () => {
   const [name, setName] = useState('');
@@ -11,41 +11,45 @@ const RegisterUser = () => {
   const loginError = useSelector((state) => state.users.loginError);
   const error = useSelector((state) => state.users.error);
   const navigate = useNavigate();
-  const handleSubmit = () => {
+
+  const handleSubmit = async () => {
     const data = {
       name,
       username: userName,
     };
-    dispatch(createUser(data));
-    navigate('/login');
+
+    try {
+      await dispatch(createUser(data)).unwrap(); // Wait for the createUser action to complete successfully
+      navigate('/login'); // Navigate to the login page if successful
+    } catch (error) {
+      // Handle the error
+      console.error(error.message);
+    }
   };
+
   return (
     <form action="log-in" method="post" className="form-container">
       <div className="login-container">
-        <label htmlFor="registerName" className="label-text">
-          Name
-          <input
-            className="username-field"
-            type="text"
-            name="registerName"
-            id="registerName"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label htmlFor="registerUserName" id="registerUserName" className="label-text">
-          Username
-          <input
-            className="username-field"
-            type="text"
-            name="registerUserName"
-            id="registerUserName"
-            placeholder="Enter your username"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </label>
+        <label htmlFor="registerName" className="label-text">Name</label>
+        <input
+          className="username-field"
+          type="text"
+          name="registerName"
+          id="registerName"
+          placeholder='Enter your name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="registerUserName" className="label-text">Username</label>
+        <input
+          className="username-field"
+          type="text"
+          name="registerUserName"
+          id="registerUserName"
+          placeholder='Enter your username'
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
         {loginError && error && <p className="error-message">{error}</p>}
         <button type="button" className="login-button" onClick={handleSubmit}>Sign In</button>
       </div>
